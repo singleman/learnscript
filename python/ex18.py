e4a2b0dd8e39749132f96aa423dbec3d7f87bf70
singleman <singleman.real@163.com>
2020-10-19 11:05:38 +0000

    1019

---
+++ python/ex18.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2} ")
+
+def print_two_again(arg1,arg2):
+    print(f"arg1: {arg1}, arg2: {arg2} ")
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+def print_none():
+    print("I got nothing")
+
+print_two("zed","shaw")
+print_two_again("aed","df")
+print_one("first")
+print_none()
