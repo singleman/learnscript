8b3c3f2bd06f0801cbadf2fa049128865d07ed02
singleman <singleman.real@163.com>
2020-10-19 14:14:27 +0000

    10192

---
+++ python/opp/person.py
@@ -0,0 +1,14 @@
+#Add incremental self-test code
+
+class Person:
+    def __init__(self, name, job = None, pay = 0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+if __name__ == '__main__':
+    #self-test code
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job= 'dev',pay=10000)
+    print(bob.name, bob.pay)
+    print(sue.name, sue.pay)
